1352dba70e799020b1207e689c214d09aeb8b4d5
unknown <shivamshah1210@gmail.com>
2023-06-11 20:09:31 +0530

    full complete

--- script.js
+++ script.js
@@ -1,31 +1,73 @@
 gsap.registerPlugin(ScrollTrigger);
 
 
-// gsap.to("#page5" ,{
-//     position:"fixed",
-//     duration:3,
-//     ease:Expo.InOut,
-//     scrollTrigger:{
-
-//         scrub:1,
-//         trigger: "#page5",
-//         start: "top",
-//         end:"0%",
-//         markers:true,
-        
-//     }
-// })
-// gsap.to( "#page6",{
-//     position:"fixed",
-//     duration:3,
-//     ease:Expo.InOut,
-//     scrollTrigger:{
-//         scrub:1,
-//         trigger: "#page6",
-//         start: "top",
-//         end:"0%",
-//         markers:true,
-        
-//     }
-
-// })
+gsap.to("#page5",{
+    duration:3,
+    position:"fixed",
+    ease:Expo.easeInOut,
+    scrollTrigger:{
+        trigger:"#page5",
+        start:"top top",
+        end:"0% end",
+        markers:true,
+       scrub:1,
+    //    anticipatePin:1,
+       pin:true
+    }
+})
+gsap.to("#page6",{
+    duration:3,
+    position:"relative",
+    ease:Expo.easeInOut,
+    scrollTrigger:{
+        trigger:"#page5",
+        start:"top top",
+        end:"0% end",
+        // markers:true,
+       scrub:1,
+       anticipatePin:1,
+       pin:true
+    }
+})
+gsap.to("#page6",{
+    duration:3,
+    position:"fixed",
+    ease:Expo.easeInOut,
+    scrollTrigger:{
+        trigger:"#page6",
+        start:"top top",
+        end:"0% end",
+        // markers:true,
+       scrub:1,
+    //    anticipatePin:1,
+       pin:true
+    }
+})
+gsap.to("#page6",{
+    duration:3,
+    position:"relative",
+    ease:Expo.easeInOut,
+    scrollTrigger:{
+        trigger:"#page7",
+        start:"top top",
+        end:"0% end",
+        // markers:true,
+       scrub:1,
+    //    anticipatePin:1,
+       pin:true
+    }
+})
+gsap.to("#page5",{
+    duration:3,
+    position:"relative",
+    ease:Expo.easeInOut,
+    scrollTrigger:{
+        trigger:"#page5",
+        start:"bottom ",
+        end:"bottom",
+        markers:true,
+       scrub:1,
+    //    anticipatePin:1,
+    //    pin:true
+    }
+})
