142a40c58eff67264b967a978606d245965818cc
unknown <shivamshah1210@gmail.com>
2023-06-11 20:14:17 +0530

    final

--- script.js
+++ script.js
@@ -9,26 +9,26 @@ gsap.to("#page5",{
         trigger:"#page5",
         start:"top top",
         end:"0% end",
-        markers:true,
+        // markers:true,
        scrub:1,
     //    anticipatePin:1,
        pin:true
     }
 })
-gsap.to("#page6",{
-    duration:3,
-    position:"relative",
-    ease:Expo.easeInOut,
-    scrollTrigger:{
-        trigger:"#page5",
-        start:"top top",
-        end:"0% end",
-        // markers:true,
-       scrub:1,
-       anticipatePin:1,
-       pin:true
-    }
-})
+// gsap.to("#page6",{
+//     duration:3,
+//     position:"relative",
+//     ease:Expo.easeInOut,
+//     scrollTrigger:{
+//         trigger:"#page5",
+//         start:"top top",
+//         end:"0% end",
+        // markers:true, 
+//        scrub:1,
+//        anticipatePin:1,
+//        pin:true
+//     }
+// })
 gsap.to("#page6",{
     duration:3,
     position:"fixed",
@@ -37,7 +37,7 @@ gsap.to("#page6",{
         trigger:"#page6",
         start:"top top",
         end:"0% end",
-        // markers:true,
+        markers:true,
        scrub:1,
     //    anticipatePin:1,
        pin:true
@@ -51,7 +51,7 @@ gsap.to("#page6",{
         trigger:"#page7",
         start:"top top",
         end:"0% end",
-        // markers:true,
+        markers:true,
        scrub:1,
     //    anticipatePin:1,
        pin:true
@@ -65,7 +65,7 @@ gsap.to("#page5",{
         trigger:"#page5",
         start:"bottom ",
         end:"bottom",
-        markers:true,
+        // markers:true,
        scrub:1,
     //    anticipatePin:1,
     //    pin:true
